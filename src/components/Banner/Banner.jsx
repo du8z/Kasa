e8c789fd734d6768e8d2509272f86b1db1222e22
Emilien Dubois <emilien.dubs72@outlook.fr>
2023-03-29 18:07:17 +0200

    Banner automatiser fini

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,17 +1,30 @@
-import image from '../../assets/imgBannerHome.png'
 
 
-
-
-
-
-export default function Banner () {
-    return (
-        <div id="bannerHome">
-        <div className='divImgBanner'><img src={image} alt="image" className='imgBanner' />
+export default function Banner (props) {
+    const Url = props.img[0]
+    if (props.img[1] !== undefined) {
+        return (
+            <div id="bannerHome">
+            <div className='divImgBanner'><img src={Url} alt={props} className='imgBanner' />
+            
+            <p className='pBanner'>{props.img[1]}</p>
+            </div>
+            
         </div>
-        
-    </div>
-      
-    )
+          
+        )
+    } else {
+        return (
+            <div id="bannerHome">
+            <div className='divImgBanner'><img src={Url} alt={props} className='imgBanner' />
+            
+            </div>
+            
+        </div>
+          
+        )
+    }
+    
+    
 }
+
